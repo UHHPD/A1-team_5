529faabec69c76027460c15ba3b4960b9c44a922
Zeyue Wu <82392821+EisblumeKs@users.noreply.github.com>
2021-04-13 07:12:22 +0200

    Update summieren.cc

---
+++ summieren.cc
@@ -0,0 +1,25 @@
+#include <iostream> 
+#include <fstream> 
+using namespace std;
+
+int main () { 
+
+ifstream fin ("daten.txt"); 
+ofstream fout ("datensumme.txt"); 
+int nr1; 
+int nr2; 
+int sm; 
+
+for (int zeile=1;zeile<=234;zeile++) { 
+fin >> nr1; 
+fin >> nr2; 
+sm = nr1 + nr2; 
+
+fout << sm << '\n'; 
+
+}
+
+fin.close(); 
+fout.close(); 
+
+} ; 
